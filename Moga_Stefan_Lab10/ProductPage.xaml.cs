60fea9af16afdc0e48371f9eb4d8752b948ab936
ȘTEFAN-ALEXANDRU MOGA <stefan.moga@stud.ubbcluj.ro>
2022-01-05 20:32:03 +0200

    lab11

---
+++ Moga_Stefan_Lab10/ProductPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Moga_Stefan_Lab10.Models;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Moga_Stefan_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductPage : ContentPage
+    {
+        ShopList sl;
+        public ProductPage(ShopList slist)
+        {
+            InitializeComponent();
+            sl = slist;
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var product = (Product)BindingContext;
+            await App.Database.SaveProductAsync(product);
+            listView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var product = (Product)BindingContext;
+            await App.Database.DeleteProductAsync(product);
+            listView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetProductsAsync();
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            Product p;
+            if (e.SelectedItem != null)
+            {
+                p = e.SelectedItem as Product;
+                var lp = new ListProduct()
+                {
+                    ShopListID = sl.ID,
+                    ProductID = p.ID
+                };
+                await App.Database.SaveListProductAsync(lp);
+                p.ListProducts = new List<ListProduct> { lp };
+
+                await Navigation.PopAsync();
+            }
+
+        }
+    }
+}
---
+++ Moga_Stefan_Lab10/ValidationBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace Moga_Stefan_Lab10
+{
+    class ValidationBehaviour : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+            string.IsNullOrEmpty(args.NewTextValue) ? Color.Red : Color.Default;
+        }
+    }
+}
